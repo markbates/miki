650e4ff764c86a2642f6e4381e8f2f436f9d51b6
Mark Bates <mark@markbates.com>
2008-08-18 17:30:24 -0400

    Added thin.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,13 +10,13 @@ set :repository,  "git://github.com/markbates/miki.git" # your repository. this 
 set :scm, "git"
 set :user, "markbates" #github username
 set :branch, "master" #this is the branch you want. most likely master
-# set :deploy_via, :remote_cache
+set :deploy_via, :remote_cache
 
 #from hostingrails.com/forums/wiki_thread/46
 set :use_sudo, false
 set :port, 22  # your port on slicehost. Standard port for ssh is 22, but if you followed the slicehost articles, you probably changed this to something different
 set :deploy_to, "/home/#{user}/#{application}"          # Where on the server your app will be deployed
-set :chmod755, "app config log db lib public vendor script script/* public/disp*"  	# Some files that will need proper permissions
+set :chmod755, "app config tmp log db lib public vendor"  	# Some files that will need proper permissions
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -36,20 +36,33 @@ task :reload_nginx do
   sudo "/etc/init.d/nginx reload"
 end
 
-desc "Restart Thin"
-task :restart_thin do
-  run "cd /home/#{user}/#{application}/current && thin restart -s 3 -e production && pwd"
-  # sudo "thin restart -s 3 -e production && pwd"
-end
-
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
-    # try_runner "#{current_path}/script/process/reaper"
+    # sudo "thin restart -s 3 -e production -c /home/#{user}/#{application}/current/"
+    sudo "thin restart -C /home/#{user}/#{application}/current/config/thin.yml -c /home/#{user}/#{application}/current/"
+  end
+  task :start, :roles => :app do
+    sudo "thin start -s 3 -e production -c /home/#{user}/#{application}/current/"
+  end
+  
+  task :migrate, :roles => :db, :only => { :primary => true } do
+    rake = fetch(:rake, "rake")
+    rails_env = fetch(:rails_env, "production")
+    migrate_env = fetch(:migrate_env, "")
+    migrate_target = fetch(:migrate_target, :latest)
+
+    directory = case migrate_target.to_sym
+      when :current then current_path
+      when :latest  then current_release
+      else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+      end
+
+    run "cd #{directory}; #{rake} MACK_ENV=#{rails_env} #{migrate_env} db:migrate"
   end
 end
 
 # after "deploy", "deploy:cleanup"
 # after "deploy:cleanup", "reload_nginx"
-after "restart_thin", "reload_nginx"
+# after "restart_thin", "reload_nginx"
 
-after "deploy", "restart_thin"
+after "deploy", "reload_nginx"
