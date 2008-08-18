3b0b4dc5ee7a024f1b744c0ed37e3cbcbc01d6bd
Mark Bates <mark@markbates.com>
2008-08-18 17:17:34 -0400

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :branch, "master" #this is the branch you want. most likely master
 set :use_sudo, false
 set :port, 22  # your port on slicehost. Standard port for ssh is 22, but if you followed the slicehost articles, you probably changed this to something different
 set :deploy_to, "/home/#{user}/#{application}"          # Where on the server your app will be deployed
-set :chmod755, "app config db lib public vendor script script/* public/disp*"  	# Some files that will need proper permissions
+set :chmod755, "app config log db lib public vendor script script/* public/disp*"  	# Some files that will need proper permissions
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -33,16 +33,23 @@ role :db,  domain, :primary => true
 
 desc "Reload Nginx"
 task :reload_nginx do
-  sudo "nginx restart"
+  sudo "/etc/init.d/nginx reload"
 end
 
 desc "Restart Thin"
 task :restart_thin do
-  sudo "thin restart -s 3 -e production"
+  run "cd /home/#{user}/#{application}/current && thin restart -s 3 -e production && pwd"
+  # sudo "thin restart -s 3 -e production && pwd"
+end
+
+namespace :deploy do
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    # try_runner "#{current_path}/script/process/reaper"
+  end
 end
 
 # after "deploy", "deploy:cleanup"
 # after "deploy:cleanup", "reload_nginx"
-# after "reload_nginx", "restart_thin"
+after "restart_thin", "reload_nginx"
 
 after "deploy", "restart_thin"
