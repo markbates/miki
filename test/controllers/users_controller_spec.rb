ebe6f24b414089f0ee88b48ccebf57630c0a475e
Mark Bates <mark@markbates.com>
2008-08-22 17:08:58 -0400

    Added some more user controller specs.

--- test/controllers/users_controller_spec.rb
+++ test/controllers/users_controller_spec.rb
@@ -2,15 +2,27 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 describe UsersController do
   
+  before(:each) do
+    @user = User.first
+  end
+  
   describe "index" do
     
-    it "should list users"
+    it "should list users" do
+      get users_index_url
+      response.should be_successful
+      assigns(:users).should == User.all
+    end
     
   end
   
   describe "show" do
     
-    it "should show a user"
+    it "should show a user" do
+      get users_show_url(:id => @user)
+      response.should be_successful
+      assigns(:user).should == @user
+    end
     
   end
   
@@ -27,7 +39,20 @@ describe UsersController do
   
   describe "edit" do
     
-    it "should edit a user"
+    it "should require login" do
+      get users_edit_url(:id => @user)
+      response.should be_redirected_to(login_url)
+    end
+    
+    it "should require the owner to edit it" do
+      session[:user_id] = @user.id
+      get users_edit_url(:id => @user)
+      response.should be_successful
+      assigns(:user).should == @user
+      
+      get users_edit_url(:id => User.first(:id.not => @user.id))
+      response.should be_redirected_to(login_url)
+    end
     
   end
   
@@ -44,7 +69,21 @@ describe UsersController do
   
   describe "update" do
     
-    it "should update a user"
+    it "should require login" do
+      put users_update_url(:id => @user)
+      response.should be_redirected_to(login_url)
+    end
+    
+    it "should require the owner to edit it" do
+      session[:user_id] = @user.id
+      put users_update_url(:id => @user), :user => {:display_name => "Mark Bates"}
+      response.should be_redirected_to(users_show_url(:id => @user))
+      assigns(:user).should == @user
+      assigns(:user).display_name.should == "Mark Bates"
+      
+      put users_update_url(:id => User.first(:id.not => @user.id))
+      response.should be_redirected_to(login_url)
+    end
     
   end
   
