649aa0fa9ef2a7048d7452361337b81445411d0f
Mark Bates <mark@mackframework.com>
2008-08-22 23:15:51 -0400

    Changed clean_url to clean even more.

--- lib/common_helpers.rb
+++ lib/common_helpers.rb
@@ -2,10 +2,10 @@ module Miki
   module CommonHelpers
     
     def clean_url(url)
-      url = url.to_s
-      url = url[1..url.size] if url.starts_with?('/')
-      url.gsub!(' ', '_')
-      url.downcase
+      # url = url.to_s
+      # url = url[1..url.size] if url.starts_with?('/')
+      # url.gsub!(' ', '_')
+      url.methodize.downcase
     end
     
     include_safely_into(Mack::Rendering::ViewTemplate, Mack::Controller, DataMapper::Resource)
