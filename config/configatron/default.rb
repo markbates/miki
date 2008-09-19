2724d4d799a9c550c05e223a6e1c603f4b36306a
Mark Bates <mark@markbates.com>
2008-09-19 16:38:07 -0400

    Upgraded to the latest version of configatron

--- config/configatron/default.rb
+++ config/configatron/default.rb
@@ -1,7 +1,3 @@
-configatron do |c|
-  c.namespace(:mack) do |mack|
-    mack.session_id = '_miki_session_id'
-    mack.js_framework = :prototype
-  end
-  c.default_secret_key = '4K45YJ5ZZQRX3QYW5KBEHC5LSH7G4X7ZLZNFDN4W6PD87BE4D487D7DXGM7SRP4ETNCPWPDEHW8RYF86'
-end
+configatron.mack.session_id = '_miki_session_id'
+configatron.mack.js_framework = :prototype
+configatron.default_secret_key = '4K45YJ5ZZQRX3QYW5KBEHC5LSH7G4X7ZLZNFDN4W6PD87BE4D487D7DXGM7SRP4ETNCPWPDEHW8RYF86'
--- config/configatron/test.rb
+++
@@ -1,7 +0,0 @@
-configatron do |c|
-  c.namespace(:mack) do |mack|
-    mack.namespace(:log) do |log|
-      log.level :error
-    end
-  end
-end
